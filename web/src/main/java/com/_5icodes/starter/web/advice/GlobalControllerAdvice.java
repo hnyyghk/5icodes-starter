81aa0ac5afdef9ce9622c70dcc2fcc22b856f510
hnyyghk <i@5icodes.com>
2021-07-31 20:15:11 +0800

    fix web module

--- web/src/main/java/com/_5icodes/starter/web/advice/GlobalControllerAdvice.java
+++ web/src/main/java/com/_5icodes/starter/web/advice/GlobalControllerAdvice.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -21,8 +22,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolation;
@@ -33,6 +36,9 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * 统一异常处理
+ */
 @RestControllerAdvice
 @Slf4j
 public class GlobalControllerAdvice {
@@ -48,23 +54,14 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler({BizException.class, BizRuntimeException.class})
     public ResultDTO handleCodeException(CodeMsg codeMsg) {
-        log.warn("codeMsgException occurred, code: {}, message: {}", codeMsg.getCode(), codeMsg.getMessage());
+        log.warn("CodeMsgException occurred, code: {}, message: {}", codeMsg.getCode(), codeMsg.getMessage());
         return new ResultDTO(codeMsg.getCode(), codeMsg.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResultDTO handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        BindingResult bindingResult = e.getBindingResult();
-        List<ObjectError> allErrors = bindingResult.getAllErrors();
-        String message = allErrors.stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.joining(","));
-        return fail(message);
-    }
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResultDTO handleConstraintViolationException(ConstraintViolationException e) {
-        Set<ConstraintViolation<?>> constraintViolations = e.getConstraintViolations();
-        String message = constraintViolations.stream().map(ConstraintViolation::getMessage).collect(Collectors.joining(","));
-        return fail(message);
+        //jsr303 方法参数 校验异常处理
+        return processBindingResult(e.getBindingResult());
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
@@ -75,6 +72,8 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResultDTO handleIllegalArgumentException(IllegalArgumentException e) {
+        //非法参数异常
+//        e.getMessage();
         log.error("非法参数异常:", e);
         return fail(e.getLocalizedMessage());
     }
@@ -88,6 +87,7 @@ public class GlobalControllerAdvice {
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public ResultDTO handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
         String parameterName = e.getParameterName();
+//        e.getMessage();
         log.warn("缺少请求参数: {}", parameterName);
         return fail(String.format("缺少请求参数%s", parameterName));
     }
@@ -98,8 +98,37 @@ public class GlobalControllerAdvice {
         return fail(String.format("不支持%s请求方式", method));
     }
 
+    @ExceptionHandler(BindException.class)
+    public ResultDTO handleBindException(BindException e) {
+        //jsr303 方法参数 校验异常处理
+        return processBindingResult(e.getBindingResult());
+    }
+
+    private ResultDTO processBindingResult(BindingResult bindingResult) {
+//        bindingResult.getFieldErrors().forEach(fieldError -> {
+//            fieldError.getField();
+//            fieldError.getRejectedValue();
+//            fieldError.getDefaultMessage();
+//        });
+        List<ObjectError> allErrors = bindingResult.getAllErrors();
+        String message = allErrors.stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.joining(","));
+        return fail(message);
+    }
+
+    @ExceptionHandler({UndeclaredThrowableException.class, InvocationTargetException.class})
+    public Object handleProxyException(Exception e, HttpServletRequest request, HttpServletResponse response, HandlerMethod handlerMethod) {
+        Throwable realException = ExceptionUtils.getRealException(e);
+        if (realException instanceof Exception) {
+            return handlerExceptionResolver.resolveException(request, response, handlerMethod, (Exception) realException);
+        } else {
+            return handleThrowable(realException);
+        }
+    }
+
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResultDTO handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        //Content-Type post json 请求参数不规范处理
+//        e.getMessage();
         log.warn("请求体格式错误: {}", e.getLocalizedMessage());
         return fail("请求体格式错误");
     }
@@ -122,13 +151,27 @@ public class GlobalControllerAdvice {
         return fail(errorProperties.getMessage());
     }
 
-    @ExceptionHandler({UndeclaredThrowableException.class, InvocationTargetException.class})
-    public Object handleProxyException(Exception e, HttpServletRequest request, HttpServletResponse response, HandlerMethod handlerMethod) {
-        Throwable realException = ExceptionUtils.getRealException(e);
-        if (realException instanceof Exception) {
-            return handlerExceptionResolver.resolveException(request, response, handlerMethod, (Exception) realException);
-        } else {
-            return handleThrowable(realException);
-        }
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResultDTO handleConstraintViolationException(ConstraintViolationException e) {
+        //jsr303 validation 异常处理
+        Set<ConstraintViolation<?>> constraintViolations = e.getConstraintViolations();
+//        constraintViolations.forEach(constraintViolation -> {
+//            constraintViolation.getMessageTemplate();
+//        });
+        String message = constraintViolations.stream().map(ConstraintViolation::getMessage).collect(Collectors.joining(","));
+        return fail(message);
+    }
+
+    @ExceptionHandler(ServletException.class)
+    public ResultDTO handleServletException(ServletException e) {
+        //servlet 异常
+        log.error("servlet exception:", e);
+        return fail(e.getMessage());
+    }
+
+    @ExceptionHandler(MultipartException.class)
+    public ResultDTO handleMultipartException(MultipartException e) {
+        //文件上传大小异常 或 请求字节过大异常
+        return fail(e.getMessage());
     }
 }
