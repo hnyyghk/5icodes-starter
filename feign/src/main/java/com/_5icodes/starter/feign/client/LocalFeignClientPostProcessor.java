c262a90d6b4c6750a1425dd102aaf601f59c8ae0
hnyyghk <i@5icodes.com>
2021-12-15 03:08:20 +0800

    support feign localMapping config

--- feign/src/main/java/com/_5icodes/starter/feign/client/LocalFeignClientPostProcessor.java
+++ feign/src/main/java/com/_5icodes/starter/feign/client/LocalFeignClientPostProcessor.java
@@ -1,41 +1,33 @@
 package com._5icodes.starter.feign.client;
 
-import com._5icodes.starter.feign.utils.FeignReflectionUtils;
 import lombok.SneakyThrows;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.*;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.boot.context.properties.bind.Bindable;
 import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClientFactoryBean;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.core.Ordered;
+import org.springframework.core.annotation.AnnotationAttributes;
 import org.springframework.core.env.Environment;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Supplier;
 
 /**
  * feign url mapping for local env
+ *
+ * @see org.springframework.cloud.openfeign.FeignClientsRegistrar#registerFeignClient
  */
 public class LocalFeignClientPostProcessor implements BeanFactoryPostProcessor, EnvironmentAware, Ordered {
     private Map<String, String> mappings;
-    private static final Field NAME_FIELD;
-    private static final Field URL_FIELD;
-
-    static {
-        try {
-            Class<?> feignFactoryClass = FeignReflectionUtils.getFeignFactoryClass();
-            NAME_FIELD = feignFactoryClass.getDeclaredField("name");
-            URL_FIELD = feignFactoryClass.getDeclaredField("url");
-            NAME_FIELD.setAccessible(true);
-            URL_FIELD.setAccessible(true);
-        } catch (Exception e) {
-            throw new RuntimeException("reflect FeignClientFactoryBean failed", e);
-        }
-    }
 
     @Override
     @SneakyThrows
@@ -43,28 +35,45 @@ public class LocalFeignClientPostProcessor implements BeanFactoryPostProcessor, 
         if (CollectionUtils.isEmpty(mappings)) {
             return;
         }
-        Class<?> feignFactoryClass = FeignReflectionUtils.getFeignFactoryClass();
-        Map<String, ?> feignClientFactoryBeanMap = beanFactory.getBeansOfType(feignFactoryClass);
-        if (CollectionUtils.isEmpty(feignClientFactoryBeanMap)) {
-            return;
-        }
-        for (Map.Entry<String, ?> entry : feignClientFactoryBeanMap.entrySet()) {
-            Object value = entry.getValue();
-            String name = (String) NAME_FIELD.get(value);
-            String url = (String) URL_FIELD.get(value);
-            String urlMapping = mappings.get(name);
-            if (!StringUtils.hasText(url) && StringUtils.hasText(urlMapping)) {
-                URL_FIELD.set(value, urlMapping);
+        String[] beanNames = beanFactory.getBeanNamesForAnnotation(FeignClient.class);
+        for (String beanName : beanNames) {
+            AbstractBeanDefinition beanDefinition = (AbstractBeanDefinition) beanFactory.getBeanDefinition(beanName);
+            FeignClientFactoryBean feignClientFactoryBean = (FeignClientFactoryBean) beanDefinition.getAttribute("feignClientsRegistrarFactoryBean");
+            String contextId = feignClientFactoryBean.getContextId();
+            String localMapping = mappings.get(contextId);
+            if (StringUtils.isEmpty(localMapping)) {
+                continue;
             }
+            Supplier<?> instanceSupplier = beanDefinition.getInstanceSupplier();
+            Field field = instanceSupplier.getClass().getDeclaredField("arg$4");
+            field.setAccessible(true);
+            AnnotationAttributes annotationAttributes = (AnnotationAttributes) field.get(instanceSupplier);
+            String url = (String) annotationAttributes.get("url");
+            if (StringUtils.hasText(url)) {
+                continue;
+            }
+            annotationAttributes.put("url", localMapping);
         }
     }
 
     @Override
     public void setEnvironment(Environment environment) {
         Binder binder = Binder.get(environment);
-        BindResult<Map<String, String>> result = binder.bind("local.feign.mapping", Bindable.mapOf(String.class, String.class));
-        if (result.isBound()) {
-            mappings = result.get();
+        BindResult<Map<String, Object>> bindResult = binder.bind("feign.client.config", Bindable.mapOf(String.class, Object.class));
+        if (bindResult.isBound()) {
+            Map<String, String> mappings = new HashMap<>();
+            Map<String, Object> properties = bindResult.get();
+            for (Map.Entry<String, Object> entry : properties.entrySet()) {
+                if (entry.getValue() instanceof Map) {
+                    String localMapping = (String) ((Map<?, ?>) entry.getValue()).get("localMapping");
+                    if (StringUtils.hasText(localMapping)) {
+                        mappings.put(entry.getKey(), localMapping);
+                    }
+                }
+            }
+            if (!mappings.isEmpty()) {
+                this.mappings = mappings;
+            }
         }
     }
 
