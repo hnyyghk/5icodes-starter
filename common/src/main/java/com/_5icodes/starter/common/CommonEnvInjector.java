848d0c53bd687fe6b27c07e658e259c6e00a566d
hnyyghk <i@5icodes.com>
2022-02-16 13:58:54 +0800

    support spring.application.name in application.yml

--- common/src/main/java/com/_5icodes/starter/common/CommonEnvInjector.java
+++ common/src/main/java/com/_5icodes/starter/common/CommonEnvInjector.java
@@ -1,27 +1,23 @@
 package com._5icodes.starter.common;
 
+import com._5icodes.starter.common.utils.GrayUtils;
 import com._5icodes.starter.common.utils.PropertySourceUtils;
 import com._5icodes.starter.common.utils.SpringApplicationUtils;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.context.config.ConfigFileApplicationListener;
+import org.springframework.boot.context.config.ConfigDataEnvironmentPostProcessor;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.ConfigurableEnvironment;
 
+/**
+ * @see org.springframework.boot.context.config.ConfigDataEnvironmentPostProcessor#ORDER
+ */
 @Slf4j
 public class CommonEnvInjector extends AbstractProfileEnvironmentPostProcessor implements Ordered {
     @Override
-    protected boolean shouldProcess(ConfigurableEnvironment env, SpringApplication application) {
-        boolean shouldProcess = super.shouldProcess(env, application);
-        if (!shouldProcess) {
-            SpringApplicationUtils.setApplicationName(env.getProperty("spring.application.name"));
-        }
-        return shouldProcess;
-    }
-
-    @Override
     protected void onAllProfiles(ConfigurableEnvironment env, SpringApplication application) {
+        SpringApplicationUtils.setApplicationName(env.getProperty("spring.application.name"));
         //日期格式化
         PropertySourceUtils.put(env, "spring.jackson.date-format", "yyyy-MM-dd HH:mm:ss");
         PropertySourceUtils.put(env, "spring.jackson.time-zone", "GMT+8");
@@ -34,6 +30,6 @@ public class CommonEnvInjector extends AbstractProfileEnvironmentPostProcessor i
 
     @Override
     public int getOrder() {
-        return ConfigFileApplicationListener.DEFAULT_ORDER + 1;
+        return ConfigDataEnvironmentPostProcessor.ORDER + 1;
     }
 }
